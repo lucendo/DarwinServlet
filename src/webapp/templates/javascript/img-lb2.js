09dff94784c0ae432d782b90ff118d09da778976
Antranig Basman <amb26@ponder.org.uk>
2008-06-01 23:28:28 +0000

    Corrected Javascript event delivery issues

--- src/webapp/templates/javascript/img-lb2.js
+++ src/webapp/templates/javascript/img-lb2.js
@@ -18,41 +18,41 @@ var new_page = 0;
 var currentimage;
 
 function imageloaded() {
-//alert("imageloaded "+currentimage.width+"x"+currentimage.height);
+  //alert("imageloaded "+currentimage.width+"x"+currentimage.height);
 
-original_w = currentimage.width;
-original_h = currentimage.height;
+  original_w = currentimage.width;
+  original_h = currentimage.height;
 
-if (new_page == 1) {
-  new_page = 0;
-  clear_modes();
-  set_initial_zoom();
+  if (new_page == 1) {
+    new_page = 0;
+    clear_modes();
+    set_initial_zoom();
+    }
+  if (fit_width_mode) {
+    fit_width();
+    }
+  else if (fit_both_mode) {
+    fit_both();
+    }
+  zoom();
+  parent.frames.imageframe.document.viewed_image.src = currentimage.src;
+  //alert("rightsrc: " + parent.frames.imageframe.document.viewed_image.src);
   }
-if (fit_width_mode) {
-  fit_width();
-  }
-else if (fit_both_mode) {
-  fit_both();
-  }
-zoom();
-parent.frames.imageframe.document.viewed_image.src = currentimage.src;
-}
 
 function pageinit() {
   new_page = 1;
   }
 
 function init(imagetarget) {
- var image = new Image();
- image.onload = imageloaded;
- image.src = imagetarget;
-currentimage = image;
+  var image = new Image();
+  currentimage = image;
+  image.onload = imageloaded;
+  image.src = imagetarget;
 
-
-original_w = image.width;
-original_h = image.height;
-//parent.document.viewed_image.src = image.src;
-}
+  original_w = image.width;
+  original_h = image.height;
+  //parent.document.viewed_image.src = image.src;
+  }
 
 function clear_modes() {
   fit_width_mode = 0;
@@ -61,31 +61,31 @@ function clear_modes() {
 
 function bigger() {
     // this function increases the size of the image
-    clicks +=1;
-    clear_modes();
-    zoom();
+  clicks +=1;
+  clear_modes();
+  zoom();
 }
 
 function smaller() {
-    // this function decreases the image size
-    clicks -= 1;
-    clear_modes();  
-    zoom();
-    }
+  // this function decreases the image size
+  clicks -= 1;
+  clear_modes();  
+  zoom();
+  }
 
 function orig_size() {
-    // reverts image to its original size
-    clicks = 0;
-    clear_modes();
-    zoom();
+  // reverts image to its original size
+  clicks = 0;
+  clear_modes();
+  zoom();
 }
 
 function zoom() {
 	// this function controls the resizing of the image
-current_image = parent.frames.imageframe.document.viewed_image;
+  current_image = parent.frames.imageframe.document.viewed_image;
 
-current_image.width = original_w*Math.pow(mag_factor,clicks);
-current_image.height = original_h*Math.pow(mag_factor,clicks);
+  current_image.width = original_w*Math.pow(mag_factor,clicks);
+  current_image.height = original_h*Math.pow(mag_factor,clicks);
 }
 
 
@@ -93,31 +93,31 @@ function set_initial_zoom() {
   var fct = compute_fit_width();
   if (fct < 0) {
     fit_width();
-  }
+    }
   else {
     orig_size();
+    }
   }
-}
 
 function compute_fit_width() {
 
-var fct;
+  var fct;
     // the next 4 lines determine the magnification factor to fill the window width and applies it to the height
-mg = document.body.clientWidth - 20;
-fct = ((Math.log(mg/original_w)/Math.log(mag_factor)));
-return fct;
-}
+  mg = document.body.clientWidth - 20;
+  fct = ((Math.log(mg/original_w)/Math.log(mag_factor)));
+  return fct;
+  }
 
 function fit_width() {
     // this function resizes the image to fill the current window width
-clicks = compute_fit_width();
-if (clicks > 0) {
-  clicks = 0;
-  }
+  clicks = compute_fit_width();
+  if (clicks > 0) {
+    clicks = 0;
+    }
   
-clear_modes();
-fit_width_mode = 1;
-zoom();
+  clear_modes();
+  fit_width_mode = 1;
+  zoom();
 
 }
 
@@ -125,20 +125,20 @@ function fit_both() {
     // this function resizes the image to fill the current window width
 
     // the next 4 lines determine the magnification factor to fill the window width and applies it to the height
-mw = parent.frames.imageframe.document.body.clientWidth - 20;
-fctw = ((Math.log(mw/original_w)/Math.log(mag_factor)) );
+  mw = parent.frames.imageframe.document.body.clientWidth - 20;
+  fctw = ((Math.log(mw/original_w)/Math.log(mag_factor)) );
 
-mh = parent.frames.imageframe.document.body.clientHeight - 20;
-fcth = ((Math.log(mh/original_h)/Math.log(mag_factor)) );
+  mh = parent.frames.imageframe.document.body.clientHeight - 20;
+  fcth = ((Math.log(mh/original_h)/Math.log(mag_factor)) );
 
-fct = Math.min(fctw, fcth);
+  fct = Math.min(fctw, fcth);
 
-clicks = fct;
+  clicks = fct;
 
-if (clicks > 0) {
-  clicks = 0;
-  }
-clear_modes();
-fit_both_mode = 1;
-zoom();
+  if (clicks > 0) {
+    clicks = 0;
+    }
+  clear_modes();
+  fit_both_mode = 1;
+  zoom();
 }
